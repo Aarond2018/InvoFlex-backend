fbac6f003d7b414e09213ff3a1aaab93fe9c6b47
Aaron Damilola <aarondamilola1998@gmail.com>
2024-05-04 03:58:28 +0100

    feat: implemented API security mechanisms

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
+const helmet = require("helmet")
+const rateLimit = require("express-rate-limit")
+const mongoSanitize = require("express-mongo-sanitize")
 
 const authRoutes = require("./routes/auth-routes")
 const userRoutes = require("./routes/user-routes")
@@ -8,12 +11,24 @@ const GlobalErrorHandler = require("./middlewares/GlobalErrorHandler")
 
 const app = express();
 
+app.use(helmet())
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+const limiter = rateLimit({
+	windowMs: 30 * 60 * 1000,
+	limit: 100,
+  message: "Too many requests from this IP, please try again in an hour"
+})
+
+app.use("/api", limiter)
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(mongoSanitize())
 
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/users", userRoutes)
