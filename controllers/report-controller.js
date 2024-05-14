1bc5029d3432d71312bff110520e53f6daf8b4c3
Aaron Damilola <aarondamilola1998@gmail.com>
2024-05-14 17:53:36 +0100

    feat: generate reports endpoint

--- controllers/report-controller.js
+++ controllers/report-controller.js
@@ -1,10 +1,22 @@
-const AppError = require("../util/AppError")
+const Invoice = require("../models/InvoiceModel");
+const AppError = require("../util/AppError");
 
-exports.fetchReport = async (req, res) => {
+exports.fetchReport = async (req, res, next) => {
   try {
+    const { status, client: addressedTo, dateFrom, dateTo } = req.body;
 
+    const invoices = await Invoice.find({
+      status: status.toUpperCase(),
+      addressedTo,
+      createdAt: { $gte: dateFrom, $lte: dateTo },
+    })
+
+    res.status(200).json({
+      status: "success",
+      data: invoices
+    })
 
   } catch (error) {
-    return next(new AppError("Something went wrong!", 500))
+    return next(new AppError("Something went wrong!", 500));
   }
-}
+};
